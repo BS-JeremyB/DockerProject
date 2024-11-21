59f03efb1a86b20fbe52a8a23c8d9c6f085c0b32
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-11-21 15:51:49 +0100

    correction

--- MusicAPI/MusicAPI/settings.py
+++ MusicAPI/MusicAPI/settings.py
@@ -154,7 +154,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework_simplejwt.authentication.JWTAuthentication'),
+    'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework_simplejwt.authentication.JWTAuthentication',),
 }
 
 CORS_ALLOWED_ORIGINS = [
